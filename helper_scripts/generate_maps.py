26158cf7069ee42d48fddbe662a15b3d29d137b9
Chris Bradley <chris.bradley@cy.id.au>
2015-10-08 14:15:29 +1100

    Add helper script to generate map info json files.

---
+++ helper_scripts/generate_maps.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+import urllib.request
+import json
+
+def guess_level(name, site):
+	res_split = name.split('-')
+
+	if (len(res_split) == 3):
+		# It's a computer! Well probably anyway.
+		return res_split[1]
+
+	# Now we're not sure. Ask the user!
+	return input("What level is resource [" + name + "] in [" + site + "] located?")
+
+def generate_filename(site, level):
+	level = str(level)
+	
+	if level.isnumeric():
+		level = level.zfill(2)
+	else:
+		level = level.lower()
+
+	site = site.lower()
+
+	return site + '_' + level + '.png'
+		
+
+url = 'https://bookit.unimelb.edu.au/MyPC/Front.aspx?page=getResourceStatesAPI'
+req = urllib.request.urlopen(url)
+
+encoding = req.headers.get_content_charset()
+obj = json.loads(req.read().decode(encoding))
+
+for site in obj:
+	site_data = {
+		"info":      {},
+		"levels":    {},
+		"resources": {}
+	}
+
+
+	site_id = site["id"]
+	site_name = site["name"]
+	locations = site["locations"]
+
+	site_data["info"] = {
+		"id":   site_id,
+		"name": site_name
+	}
+
+	for location in locations:
+		loc_id = location["id"]
+		loc_name = location["name"]
+		resources = location["resources"]
+
+		for resource in resources:
+			res_id = resource["id"]
+			res_name = resource["name"]
+			res_state = resource["state"]
+
+			level = guess_level(res_name, site_name)
+
+			if not level in site_data["levels"].keys():
+				site_data["levels"][level] = {}
+
+			site_data["levels"][level]["filename"] = generate_filename(site_name, level)
+			site_data["levels"][level]["order"] = ""
+
+			site_data["resources"][res_name] = {
+				"x":     0,
+				"y":     0,
+				"level": level
+			}
+
+	filename = site_name.lower() + '.json'
+	print("Saving to " + filename)
+
+	f = open(filename, 'w')
+	f.write(json.dumps(site_data, indent=4, sort_keys=True))
+	f.close()
