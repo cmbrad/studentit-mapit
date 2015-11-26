14201e24ee5406214defe2db072f630b219ccf2f
Chris Bradley <chris.bradley@cy.id.au>
2015-11-27 02:22:55 +1100

    Better implement /library/<library_id>/levels/resources?level_id=<level_id>

--- backend/app/controllers/library_controller.rb
+++ backend/app/controllers/library_controller.rb
@@ -16,11 +16,40 @@ class LibraryController < ApplicationController
     render json: {levels: @levels}
   end
 
+  # GET /library/<library_id>/levels/resources?level_id=<level_id>
+  # See readme for sample response
   def resources
+    # Display resources grouped by levels
     @levels = Level.where(library: @library)
+
+    # Level data! Construct our custom JSON response
+    lData = []
     @levels.each do |level|
-      #level.
+      # Resource data! Need to actually return a state
+      rData = []
+      # Get all data for resources in this level
+      level.resources.each do |resource|
+        rData << {
+          name: resource.bookit_name,
+          state: 'AVAILABLE'
+        }
+      end
+      # Top level, level data
+      lData << {
+        name: level.name,
+        order: level.order,
+        resources: rData
+      }
     end
+
+    # One level up the data! Bottom up approach
+    data = {
+      id: @library.id,
+      name: @library.name,
+      levels: lData
+    }
+    # Only JSON here no time for HTML!
+    render json: data
   end
 
   def map
