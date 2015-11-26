af9888323fd13eaace3f1e27577d4ba0783cb601
Chris Bradley <chris.bradley@cy.id.au>
2015-11-26 18:24:14 +1100

    Add library and level information to seeds.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+name = 'Architecture'
+library = Library.create(site: Site.where(bookit_name: name).first, name: name)
+Level.create(library: library, name: 'B', order: 0)
+Level.create(library: library, name: 'G', order: 1)
+
+name = 'Baillieu'
+library = Library.create(site: Site.where(bookit_name: name).first, name: name)
+Level.create(library: library, name: 'LG', order: 0)
+Level.create(library: library, name: 'G', order: 1)
+Level.create(library: library, name: '1', order: 2)
+Level.create(library: library, name: '2', order: 3)
+
+name = 'Brownless'
+library = Library.create(site: Site.where(bookit_name: name).first, name: name)
+Level.create(library: library, name: 'G', order: 0)
+Level.create(library: library, name: '1', order: 1)
+Level.create(library: library, name: '2', order: 2)
+
+name = 'ERC'
+library = Library.create(site: Site.where(bookit_name: name).first, name: name)
+Level.create(library: library, name: '1', order: 0)
+Level.create(library: library, name: '2', order: 1)
+Level.create(library: library, name: '3', order: 2)
+Level.create(library: library, name: '4', order: 3)
+Level.create(library: library, name: '5', order: 4)
+
+name = 'Giblin Eunson'
+library = Library.create(site: Site.where(bookit_name: name).first, name: name)
+Level.create(library: library, name: 'G', order: 0)
+Level.create(library: library, name: 'UG', order: 1)
+Level.create(library: library, name: '1', order: 2)
+
+name = 'Law'
+library = Library.create(site: Site.where(bookit_name: name).first, name: name)
+Level.create(library: library, name: '4', order: 0)
+Level.create(library: library, name: '5', order: 1)
+
+name = 'Lenton Parr'
+library = Library.create(site: Site.where(bookit_name: name).first, name: name)
+Level.create(library: library, name: '1', order: 0)
